277fda0d0daea14d046adef005e6b9764df9d1e8
KhoaPC <luongvankhoapc@gmail.com>
2022-11-29 20:01:57 +0700

    22Nov29 UPDATE Router -> HashRouter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./App.css";
 import Nav from "./deafaultLayout/Header/Header";
 import Home from "./pages/Home/Home";
 import DemoOTP from "./pages/DemoOTP/DemoOTP";
 import Page404 from "./pages/Page404/Page404";
-import ZoomImg from "./component/ZoomImg/ZoomImg";
+import DemoImg from "./pages/DemoImg/DemoImg";
+ 
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/OTP" element={<DemoOTP />}></Route>
-        <Route path="/img" element={<ZoomImg />}></Route>
+        <Route path="/img" element={<DemoImg />}></Route>
         <Route path="/*" element={<Page404 />}></Route>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 {
