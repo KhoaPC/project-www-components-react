c19c1d0392f3314ca4e50bade0d5c08f3ca8f276
KhoaPC <luongvankhoapc@gmail.com>
2022-11-30 08:14:51 +0700

    22Nov30 UPDATE layout page `DemoOTP`

--- src/pages/DemoOTP/DemoOTP.js
+++ src/pages/DemoOTP/DemoOTP.js
@@ -1,66 +1,33 @@
 import "./DemoOTP.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import OTP from "../../component/OTP/OTP";
-import NumInput from "./NumInput";
 import Form from "./Form";
-import PlaceholderInput from "./PlaceholderInput";
-import SeparatorInput from "./SeparatorInput";
-import Form2 from "./Form2";
 
 function DemoOTP() {
   const [num, setNum] = useState();
   const [separator, setSeparator] = useState();
   const [placeholder, setPlaceholder] = useState();
-  const [value, setValue] = useState();
 
   return (
     <div className="container">
       <nav className="navbar">
-        <Form2
-          label="test"
-          type="input"
-          numMax={10}
-          todoOnChange={(value) => console.log(value)}
-        />
         <Form
-          value={value}
-          callBack={(val) => {
-            setValue(val);
-          }}
-          lable="Input"
-        />
-        <Form
-          value={value}
-          callBack={(val) => {
-            setValue(val);
-          }}
-          lable="Input NumX"
-          numMax={10}
+          label="Input Num"
           type="number"
+          numMax={10}
+          todoOnChange={(value) => setNum(value)}
         />
 
-        <NumInput
-          value={num}
-          callBack={(val) => {
-            setNum(val);
-          }}
-          numMax={10}
-          lable="Input Num"
-          type="number"
+        <Form
+          label="Separator"
+          type="text"
+          todoOnChange={(value) => setSeparator(value)}
         />
-        <SeparatorInput
-          value={separator}
-          callBack={(val) => {
-            setSeparator(val);
-          }}
-          lable="Separator"
-        />
-        {/* <PlaceholderInput
-          value={placeholder}
-          callBack={(val) => {
-            setPlaceholder(val);
-          }}
-          lable="Placeholder"
+
+        {/* <Form
+          label="Placeholder"
+          type="text"
+          todoOnChange={(value) => setPlaceholder(value)}
         /> */}
       </nav>
 
