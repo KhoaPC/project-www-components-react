8142fb8938ef3f4c14ef88bc2498e948a2df50c3
KhoaPC <luongvankhoapc@gmail.com>
2022-12-23 08:12:48 +0700

    22Dec23 UPDATE checkOTP #2

--- src/pages/DemoOTP/DemoOTP.js
+++ src/pages/DemoOTP/DemoOTP.js
@@ -10,18 +10,8 @@ function DemoOTP() {
   const [isNumber, setIsNumber] = useState(false);
   const [hasErrored, setHasErrored] = useState(false);
   const [isPassword, setIsPassword] = useState(false);
-  const [checkOutput, setCheckOutput] = useState(false);
   const [randomOTP, setOTP] = useState(0);
   const [output, setOutput] = useState(Array.from({ length: num }));
-
-  useEffect(() => {
-    if (checkOutput) {
-      const result = +output.join("");
-      if (result === randomOTP && output.length > 0) {
-        alert("Correct");
-      }
-    } else return;
-  }, [checkOutput, output]);
 
   const randomNum = (maxLength) => {
     setOTP(Math.floor(Math.random() * "1".padEnd(+maxLength + 1, 0)));
@@ -67,13 +57,9 @@ function DemoOTP() {
             type="checkbox"
             todoOnChange={(value) => setHasErrored(value)}
           />
-          <Form
-            label="checkOutput"
-            type="checkbox"
-            todoOnChange={(value) => setCheckOutput(value)}
-          />
         </nav>
         <OTP
+          otp={randomOTP}
           output={output}
           setOutput={setOutput}
           inputNum={num}
@@ -82,18 +68,7 @@ function DemoOTP() {
           isPassword={isPassword}
           hasErrored={hasErrored}
           isDisable={isDisable}
-          checkOutput={checkOutput}
         ></OTP>
-      </div>
-      <div className="info">
-        <div className="info-input">
-          <h1 className="lable">OTP:</h1>
-          <h1 className="result">{randomOTP}</h1>
-        </div>
-        <div className="info-output">
-          <h1 className="lable">Output:</h1>
-          <h1 className="result">{output.join("") || 0}</h1>
-        </div>
       </div>
     </>
   );
